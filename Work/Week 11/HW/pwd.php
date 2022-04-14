aa27a9e840eaed08f7747f1ef871a8d28b41adee
emonzon42 <elijah.monzon42@gmail.com>
2022-04-14 16:15:29 -0400

    completed hw week 11

--- Work/Week 11/HW/pwd.php
+++ Work/Week 11/HW/pwd.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = ""; // Fill this with DB Host (localhost)
-$username = ""; // FIll this with your DB User
-$password = ""; // Fill this with password created during database setup
-$db = ""; // Fill this with DB Name
+$servername = "localhost"; // Fill this with DB Host (localhost)
+$username = "id18493471_bigmoneyeli"; // FIll this with your DB User
+$password = "baDtob-6kozxo-hakkok"; // Fill this with password created during database setup
+$db = "id18493471_week11"; // Fill this with DB Name
 
 ?>
